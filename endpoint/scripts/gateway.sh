2b719e5d667d0d6401c5f5bd64e768a287799fe8
Fieran Mason-Blakley <fmason@w134-87-147-192.wireless.uvic.ca>
2014-11-03 11:51:02 -0800

    changed gateway names to endpoint

--- endpoint/scripts/gateway.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/app/query-gateway/
-chpst -u app bash /home/app/query-gateway/runme.sh
--- util/provision.sh
+++ util/provision.sh
@@ -12,10 +12,6 @@ resize2fs /dev/mapper/vg_vagrant-lv_root
 yum remove -y vim-minimal # Removed sudo :(
 yum install -y sudo zsh vim-enhanced docker-io tmux cmake java-1.8.0-openjdk mongodb git
 
-# Leiningen
-#curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > /usr/bin/lein
-#chmod +x /usr/bin/lein
-
 # Daemons
 systemctl enable docker
 systemctl start docker
@@ -23,20 +19,13 @@ systemctl start docker
 # Add user to docker group.
 gpasswd -a vagrant docker
 
-# Set up some dotfiles
 su vagrant << EOF
-  git clone https://github.com/Hoverbear/dotfiles
+  # set up dotfiles
+  git clone https://github.com/PhysiciansDataCollaborative/dotfiles
+  
+  # get the environtment repo
+  git clone https://github.com/PhysiciansDataCollaborative/scoop-env scoop-env
 EOF
-
-# Get the Repos
-su vagrant << EOF
-  git clone https://github.com/Hoverbear/scoop-env scoop-env
-  #git clone https://github.com/PhysiciansDataCollaborative/visualizer visualizer
-  #git clone https://github.com/PhysiciansDataCollaborative/hub-api hub-api
-EOF
-#these are the deprecated closer provider and visualizer
-#git clone https://github.com/Hoverbear/scoop-provider-clj.git scoop-env/provider
-#git clone https://github.com/Hoverbear/scoop-visualizer-clj.git scoop-env/visualizer
 
 echo ''
 echo '`cd dotfiles && make` if you would like nicer configs.'
